bf423a705a082a4ff20c9449ecd5cd69c381ab6e
Josh Ferge <josh.ferge@sentry.io>
2021-06-28 11:39:36 -0700

    scim api docs in progress

---
+++ tests/apidocs/endpoints/scim/test_user_details.py
@@ -0,0 +1,22 @@
+from django.test.client import RequestFactory
+from django.urls import reverse
+
+from tests.apidocs.util import APIDocsTestCase
+from tests.sentry.api.test_scim import SCIMTestCase
+
+
+class SCIMMemberDetailsDocs(APIDocsTestCase, SCIMTestCase):
+    def setUp(self):
+        super().setUp()
+        self.member = self.create_member(user=self.create_user(), organization=self.organization)
+
+        self.url = reverse(
+            "sentry-api-0-organization-scim-member-details",
+            kwargs={"organization_slug": self.organization.slug, "member_id": self.member.id},
+        )
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        request = RequestFactory().get(self.url)
+        print(response)
+        self.validate_schema(request, response)
